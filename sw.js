0d031b419231a7c59caa8dda6e8dca0de47e5316
superjo5 <superjowtf@hotmail.com>
2026-06-27 23:29:56 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'thai-v1';
+const CACHE = 'thai-v2';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
